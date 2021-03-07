c738589a23362d796f14b850a73665b7da656d0f
Lalina Kairova <klea0840@gmail.com>
2021-03-07 16:56:13 +0400

    3.3. Разбор первой программы. [#488545]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Kairova Lali");
+        System.out.println("07.03.2021");
+    }
+}
