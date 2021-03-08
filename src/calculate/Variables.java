816f696ae2c6b7f0fb15ffd9f35571e81a5c4e14
Lalina Kairova <klea0840@gmail.com>
2021-03-08 22:16:03 +0400

    3. Арифметические операции. [#488538]

---
+++ src/calculate/Variables.java
@@ -0,0 +1,36 @@
+package calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+
+//        int age = 18;
+        float length = 73.0332f;
+        boolean baby = true;
+
+//        Если возраст человека в гг
+        byte age = 23;
+
+//        Пол
+        char gender = 'f';
+
+//        Рост в см
+        short height = 180;
+
+//        Кирпичи
+        int bricks = 2007851;
+
+//        Бактерии
+        long bacteria = 9823512546974L;
+
+//        Космические расстояния
+        double parsec = 15.8;
+
+//        Разрешение на работу
+        boolean workPermit = true;
+
+//        Размер файла
+        long fileSize = 16;
+
+
+    }
+}
