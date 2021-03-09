5b581392353bfa850cf27fd3a513ff347c6964d2
Lalina Kairova <klea0840@gmail.com>
2021-03-09 21:24:39 +0400

    4.1. Аргументы [#488536]

---
+++ src/calculator/ArgMethod.java
@@ -0,0 +1,27 @@
+package calculator;
+
+public class ArgMethod {
+
+//    public static void hello(String name) {
+//        System.out.println("Hello, " + name);
+//    }
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", your age is " + age);
+    }
+
+//    public static void hello(int age) {
+//        System.out.println("Hello, your age is " + age);
+//    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+
+        ArgMethod.hello(name, age);
+//          Другой вариант исправления
+//        ArgMethod.hello(age);
+
+//        ArgMethod.hello(name);
+    }
+}
