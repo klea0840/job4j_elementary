f73bb8c42310fb220560009c92a0f43120d09321
Lalina Kairova <klea0840@gmail.com>
2021-03-10 12:47:08 +0400

    9.1. String [#488535] correction

--- src/calculator/Fit.java
+++ src/calculator/Fit.java
@@ -3,8 +3,7 @@ package calculator;
 public class Fit {
 
         public static double manWeight(short height) {
-            double rsl = (height - 100) * 1.15;
-            return rsl;
+            return (height - 100) * 1.15;
         }
 
         public static double womanWeight(short height) {
