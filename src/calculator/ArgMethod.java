0b79a53c21d2daa8b4c154c4eb297796309fd9a5
Lalina Kairova <klea0840@gmail.com>
2021-03-10 12:44:16 +0400

    9.1. String [#488535] correction

--- src/calculator/ArgMethod.java
+++ src/calculator/ArgMethod.java
@@ -2,21 +2,21 @@ package calculator;
 
 public class ArgMethod {
 
-//    public static void hello(String name) {
-//        System.out.println("Hello, " + name);
-//    }
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
 
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", your age is " + age);
     }
 
-//    public static void hello() {
-//        System.out.println("Hello");
-//    }
+    public static void hello() {
+        System.out.println("Hello");
+    }
 
-//    public static void hello(int age) {
-//        System.out.println("Hello, your age is " + age);
-//    }
+    public static void hello(int age) {
+        System.out.println("Hello, your age is " + age);
+    }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
@@ -24,10 +24,10 @@ public class ArgMethod {
 
         ArgMethod.hello(name, age);
 //          Другой вариант исправления
-//        ArgMethod.hello(age);
+        ArgMethod.hello(age);
 
-//        ArgMethod.hello(name);
+        ArgMethod.hello(name);
 
-//        ArgMethod.hello();
+        ArgMethod.hello();
     }
 }
