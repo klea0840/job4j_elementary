aa90e485693113754b625e3c8fbcc24aa98e9559
Lalina Kairova <klea0840@gmail.com>
2021-03-11 23:35:44 +0400

    4.2. Тернарное сравнение [#488485]

---
+++ src/condition/Max.java
@@ -0,0 +1,21 @@
+package condition;
+
+public class Max {
+
+    public static int max(int left, int right) {
+        boolean which = left > right;
+        return which ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int check1 = Max.max(5, 20);
+        System.out.println(check1);
+        int check2 = Max.max(0, 0);
+        System.out.println(check2);
+        int check3 = Max.max(50, 20);
+        System.out.println(check3);
+        int check4 = Max.max(510, 510);
+        System.out.println(check4);
+
+    }
+}
