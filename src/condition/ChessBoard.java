ff94e45e6e923e7d7171d62fb3b9e220e4026593
Lalina Kairova <klea0840@gmail.com>
2021-03-13 13:35:22 +0400

    4.3.6. Math.abs [#488484] corr

--- src/condition/ChessBoard.java
+++ src/condition/ChessBoard.java
@@ -4,11 +4,7 @@ public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
 
-        if ((x1 < 0 || x1 > 7) || (y1 < 0 || y1 > 7)) {
-            return -1;
-        } else if ((x2 < 0 || x2 > 7) || (y2 < 0 || y2 > 7)) {
-            return -1;
-        } else if ((x1 == x2) || (y1 == y2) || (Math.abs(x1 - x2) != Math.abs(y1 - y2))) {
+            if (Math.abs(x1 - x2) != Math.abs(y1 - y2)) {
             return 0;
         }
         return Math.abs(y1 - y2);
@@ -16,11 +12,9 @@ public class ChessBoard {
 
     public static void main(String[] args) {
 
-        int path = ChessBoard.way(-5, 0, 5, 4);
+        int path = ChessBoard.way(-4, 0, 0, 4);
         if (path == 0) {
             System.out.println("Incorrect move");
-        } else if (path == -1) {
-            System.out.println("Incorrect data");
         } else {
             System.out.println("The number of moves: " + path);
         }
--- src/condition/Point.java
+++ src/condition/Point.java
@@ -10,7 +10,7 @@ public class Point {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) is " + result);
 
-        System.out.println("result (1, 1) to (1, 1) is " + Point.distance(1, 1, 1, 1));
+        System.out.println("result (1, 1) to (1, 1) is " + Point.distance(3, 5, 2, 4));
         System.out.println("result (2, 2) to (1, 1) is " + Point.distance(2, 2, 1, 1));
         System.out.println("result (3, 1) to (25, 1) is " + Point.distance(3, 1, 25, 25));
         System.out.println("result (-1, -8) to (-15, -25) is " + Point.distance(-1, -8, -15, -25));
