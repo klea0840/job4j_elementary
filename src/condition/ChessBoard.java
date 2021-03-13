cd5f2dc46da490c8696c867f6ea94c51eac1f22a
Lalina Kairova <klea0840@gmail.com>
2021-03-13 13:36:02 +0400

    4.3.6. Math.abs [#488484] corrr

--- src/condition/ChessBoard.java
+++ src/condition/ChessBoard.java
@@ -3,7 +3,6 @@ package condition;
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
-
             if (Math.abs(x1 - x2) != Math.abs(y1 - y2)) {
             return 0;
         }
