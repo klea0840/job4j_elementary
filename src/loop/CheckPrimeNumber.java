01f282f216ce5f31cbd03d154996ab788895b13b
Lalina Kairova <klea0840@gmail.com>
2021-03-14 18:18:41 +0400

    5.5. Простое число [#488585]

--- src/loop/CheckPrimeNumber.java
+++ src/loop/CheckPrimeNumber.java
@@ -3,14 +3,13 @@ package loop;
 public class CheckPrimeNumber {
 
         public static boolean check(int number) {
-            boolean prime = false;
-            for (int i = 2; i <= number - 1; i++) {
-                if (number % i != 0) {
-                    prime = true;
-                } else {
-                    break;
+            boolean prime = number > 1;
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
                 }
             }
             return prime;
         }
+
 }
