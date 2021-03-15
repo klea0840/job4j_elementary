364abb5a278ba662031348070c7ba6ac21f293ff
Lalina Kairova <klea0840@gmail.com>
2021-03-15 21:58:34 +0400

    6.0.1.1. Размер массива [#488517]

--- src/array/ArrayDefinition.java
+++ src/array/ArrayDefinition.java
@@ -5,7 +5,12 @@ public class ArrayDefinition {
     public static void main(String[] args) {
 
         short[] ages = new short[10];
+        System.out.println("The length of ages: " + ages.length);
+
         String[] surnames = new String[100500];
+        System.out.println("The length of surnames: " + surnames.length);
+
         float[] prices = new float[40];
+        System.out.println("The length of prices: " + prices.length);
     }
 }
